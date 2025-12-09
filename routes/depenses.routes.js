2bfdc7ffb4c03da5d802b1f537d361529c8f7d31
tatianablsc <ttblsc6@gmail.com>
2025-12-09 10:49:17 +0100

    Update depenses.routes.js
    ajout aggregation

--- routes/depenses.routes.js
+++ routes/depenses.routes.js
@@ -1,7 +1,8 @@
 
 import express from "express";
 import mongoose from "mongoose";
-import Depense from "../models/depense.js";
+import Depense from "../models/Depense.js";
+import Achat from "../models/Achat.js";
 
 const router = express.Router();
 
@@ -113,6 +114,43 @@ router.delete("/:id", async (req, res, next) => {
     }
 
     res.json({ message: "Catégorie supprimée" });
+
+  } catch (err) {
+    next(err);
+  }
+});
+
+router.get("/stats/usage", async (req, res, next) => {
+  try {
+    const stats = await Achat.aggregate([
+      {
+        $group: {
+          _id: "$depense",
+          totalAmount: { $sum: "$amount" },
+          countAchats: { $sum: 1 }
+        }
+      },
+      {
+        $lookup: {
+          from: "depenses",       
+          localField: "_id",
+          foreignField: "_id",
+          as: "depenseInfo"
+        }
+      },
+      { $unwind: "$depenseInfo" },
+      {
+        $project: {
+          _id: 0,
+          depenseId: "$depenseInfo._id",
+          depenseName: "$depenseInfo.name",
+          totalAmount: 1,
+          countAchats: 1
+        }
+      }
+    ]);
+
+    res.json(stats);
 
   } catch (err) {
     next(err);
